1c34f9212b5a70844f712a26a979f8d8c7a216d9
刘瑞麒 <liuruiqi@richs-macpro.local>
2020-11-27 15:31:44 +0800

    mybatis

--- spark/src/main/java/mapper/GraphMapper.java
+++ spark/src/main/java/mapper/GraphMapper.java
@@ -1,15 +1,16 @@
 package mapper;
 
 import entity.GraphItem;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface GraphMapper {
-    public boolean hasNode(String nodeName);
-    public void insertNode(String nodeName);
-    public boolean hasEdge(String source,String target);
-    public void insertEdge(String source,String target,float value);//添加一条边
-    public void setEdge(String source,String target,float value);//设置边的权重
-    public void updateEdge(String source,String target,float value);//更新边的权重
+    public boolean hasNode(@Param("nodeName") String nodeName);
+    public void insertNode(@Param("nodeName") String nodeName);
+    public boolean hasEdge(@Param("source") String source,@Param("target") String target);
+    public void insertEdge(@Param("source")String source,@Param("target") String target, @Param("value")float value);//添加一条边
+    public void setEdge(@Param("source")String source,@Param("target")String target,@Param("value")float value);//设置边的权重
+    public void updateEdge(@Param("source")String source,@Param("target")String target,@Param("value")float value);//更新边的权重
     public List<GraphItem> selectAll();
 }
